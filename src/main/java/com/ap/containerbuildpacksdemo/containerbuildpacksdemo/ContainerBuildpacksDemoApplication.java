a045a926736287249b9d3445cd6767fa777b6e95
arun patil <arunpatilmailbox@gmail.com>
2023-07-11 14:16:36 -0600

    initial push

---
+++ src/main/java/com/ap/containerbuildpacksdemo/containerbuildpacksdemo/ContainerBuildpacksDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ap.containerbuildpacksdemo.containerbuildpacksdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerBuildpacksDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContainerBuildpacksDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ap/containerbuildpacksdemo/containerbuildpacksdemo/ContainerBuildpacksDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.containerbuildpacksdemo.containerbuildpacksdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerBuildpacksDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
